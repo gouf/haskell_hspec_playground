80333646436989ff98ee786ee7931b089dd95eee
gouf <innocent.zero@gmail.com>
2023-12-02 22:27:51 +0900

    テストファイルの移動

--- test/Sample/ExampleSpec.hs
+++ test/Sample/ExampleSpec.hs
@@ -1,15 +1,16 @@
-module ExampleSpec
+module Sample.ExampleSpec (spec)
 where
 
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
 
-import Example
+-- FIXME: error: Could not find module ‘Sample.Example’
+-- import qualified Sample.Example as E
 
 main :: IO ()
 
 main = hspec $ do
   describe "add" $ do
     it "adds two numbers" $ do
-      add 1 2 `shouldBe` 3
+      E.add 1 2 `shouldBe` 3
